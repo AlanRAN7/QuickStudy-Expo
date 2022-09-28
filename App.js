94147b1b0a81a15dc50eafa1cdd6428e5b9f1b02
Jorge Bernal <jorgealejandrobernal@gmail.com>
2022-09-28 14:52:15 -0500

    Added lookup and modal-alert components

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import ModalComponent from './src/components/ModalComponent';
+import BuscadorComponent from './src/components/BuscadorComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* <ModalComponent/> */}
+      <BuscadorComponent/>
       <StatusBar style="auto" />
     </View>
   );
