716ce6891341822edd90398e3a847f5cf2ddf678
Jorge Bernal <jorgealejandrobernal@gmail.com>
2022-11-06 20:45:18 -0600

    Componentes terminados

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 // import ModalComponent from './src/components/ModalComponent';
 import BuscadorComponent from './src/components/BuscadorComponent';
 import DropdownFilter from "./src/components/DropdownFilter.js";
 import CardCuestionario from "./src/components/CardCuestionario.js";
+import CardPregunta from "./src/components/CardPregunta.js";
+import ModalWrong from "./src/components/ModalWrong.js";
 
 export default function App() {
   return (
@@ -13,6 +15,10 @@ export default function App() {
       <BuscadorComponent/>
       <DropdownFilter />
       <CardCuestionario />
+      <CardPregunta/>
+      {/* <Button title="Modal -> Incorrecto"/> */}
+      <ModalWrong/>
+
       <StatusBar style="auto" />
     </View>
   );
